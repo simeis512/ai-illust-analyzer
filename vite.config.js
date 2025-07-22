53f93404ff25246824265286d9a636603be0ed0c
simeis512 <simeis512@gmail.com>
2025-07-22 22:51:05 +0900

    Fix-asset-paths-for-GitHub-Pages

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/ai_illust_analyzer/",
+  base: "./",
   plugins: [
     vue(),
     vueDevTools(),
